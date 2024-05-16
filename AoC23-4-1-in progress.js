3256be70853ba825f974a1039392fb89b32092f8
Dafydd Carver-Williams <102929465+docw28@users.noreply.github.com>
2024-05-16 10:50:28 +0100

    Update AoC23-4-1-in progress.js
    added split and cleanup of cards.

--- AoC23-4-1-in progress.js
+++ AoC23-4-1-in progress.js
@@ -10,27 +10,44 @@ Card 4: 41 92 73 84 69 | 59 84 76 51 58  5 54 83
 Card 5: 87 83 26 28 32 | 88 30 70 12 93 22 82 36
 Card 6: 31 18 13 56 72 | 74 77 10 23 35 67 36 11`;
 
-cards = input_string.split("\n");
+pile = input_string.split("\n");
 
-for (card = 0; card<cards.length; card++) {
-    cards[card] = cards[card].slice(8).split(" | ");
-    cards[card][0] = cards[card][0].split();
-    cards[card][1] = cards[card][1].split();
+for (card = 0; card<pile.length; card++) {
+    pile[card] = pile[card].slice(8).split(" | ");
+    pile[card][0] = pile[card][0].split();
+    pile[card][1] = pile[card][1].split();
 }
+console.log(pile);
 
 // Need to check second array for matches in first array
 total_score = 0;
-matches = cards[0][0].filter(element => cards[0][1].includes(element) !== -1);
-matches[0] = matches[0].split(" ");
-if (matches[0].length == 0) {
-    total_score += 0;
-} else if (matches[0].length == 1) {
-    total_score += 1;
-} else {
-    total_score += 1*(2**(matches[0].length - 2));
+matches = [];
+num = 0, win = 1; // First and second elements in a card
+
+// Splitting numbers and wins into individual values
+// Removing blank entries left by double spaces
+for (card = 0; card<pile.length; card++) {
+    
+    pile[card][num] = String(pile[card][num]).split(" "); // Split and clean nums
+    for (i = 0; i<pile[card][num].length; i++) {
+        if (pile[card][num][i] == "") {
+            pile[card][num].splice(i, 1);
+        }
+    }
+    
+    pile[card][win] = String(pile[card][win]).split(" "); // Split and clean wins
+    for (i = 0; i<pile[card][win].length; i++) {
+        if (pile[card][win][i] == "") {
+            pile[card][win].splice(i, 1);
+        }
+    }
+    
+    matches = pile[card][num].filter(element => pile[card][win].includes(element));
+    console.log(matches);
+    
+    if (matches.length == 0) {
+        total_score += 0;
+    } else if (matches.length == 1) {
+        total_score += 1;
+    } else {}
 }
-
-
-console.log(cards);
-console.log(matches);
-console.log(total_score);
